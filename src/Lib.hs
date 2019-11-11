14bf8b65f8ad567668bba4d164f4f2b3f04bfcd6
Pedro Castilho <cstl.px@gmail.com>
2019-11-11 01:55:37 -0300

    Better list printing

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,23 +2,46 @@ module Lib
     ( rollMain
     ) where
 
-import Data.List ( intercalate )
-
 rollMain :: IO ()
 rollMain = do
   putStrLn $ showTerm (RollAtom "snoc")
-  putStrLn $ showTerm (RollList [RollAtom "+", RollAtom "1", RollAtom "2"])
+  putStrLn $ showTerm (RollPair
+                       (RollAtom "+")
+                       (RollPair
+                        (RollAtom "1")
+                        (RollPair
+                         (RollAtom "2")
+                         RollNil)))
   putStrLn $ showTerm (RollPair (RollAtom "car") (RollAtom "cdr"))
+  putStrLn $ showTerm (RollPair (RollAtom "1")
+                       (RollPair (RollAtom "2") (RollAtom "3")))
 
 
-data RollTerm = RollAtom String
-              | RollList [RollTerm]
+data RollTerm = RollNil
+              | RollAtom String
               | RollPair RollTerm RollTerm
 
+isList :: RollTerm -> Bool
+isList (RollPair h RollNil) = True
+isList (RollPair h t)       = isList t
+isList _                    = False
+
+printTerm :: RollTerm -> IO ()
+printTerm = putStrLn . showTerm
+
 showTerm :: RollTerm -> String
-showTerm (RollAtom s)   = s
-showTerm (RollList ls)  = parenthesize (intercalate " " $ map showTerm ls)
-showTerm (RollPair h t) = parenthesize (showTerm h ++ " . " ++ showTerm t)
+showTerm RollNil          = "()"
+showTerm (RollAtom s)     = s
+showTerm p@(RollPair _ _) = accumPairs p
+
+accumPairs :: RollTerm -> String
+accumPairs t = parenthesize $ accumPairs' t
+  where accumPairs' (RollPair h RollNil) = showTerm h
+        accumPairs' (RollPair h t)       = case t of
+          (RollPair _ _) -> showTerm h ++ " " ++ (accumPairs' t)
+          _              -> showTerm h ++ " . " ++ (showTerm t)
+
+        accumPairs' _                    = undefined
 
 parenthesize :: String -> String
 parenthesize s = "(" ++ s ++ ")"
